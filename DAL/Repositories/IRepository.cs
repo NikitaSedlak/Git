ab55a86d0ad492100c6ce17d52df141854b532df
Nikita Sedlak <nikita.sedlak312@gmail.com>
2018-05-26 20:17:58 +0300

    Add interface

---
+++ DAL/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+        T Read(int id);
+    }
+}
